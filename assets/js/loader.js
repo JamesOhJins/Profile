9b688cdc33d5b940f933974acfc033cfaea265bb
Jinsang <fldhs2012@gmail.com>
2022-02-27 21:48:03 +0900

    implemented loader for different scenarios

--- assets/js/loader.js
+++ assets/js/loader.js
@@ -1,34 +1,42 @@
 
 
 function removeLoader(source) {
-    window.addEventListener ("load", function() {
+    window.addEventListener("load", function () {
         // loader.addAttribute("img");
         // document.querySelector(".spinner > img").src = source;
         //document.querySelector(".spinner").src = source;
         // document.querySelectorAll("img[src='meh.png']");
         // var image = document.querySelector("img[src='"+source+"'");
         var url = new URL(source);
-        if(source.includes(window.location.hostname)){ //path is relative path
-        var result = url.pathname;
-        result = result.substring(9,result.length); 
+        if (source.includes(window.location.hostname) && source.includes("/Profile/")) { //path is relative path
+            //for github
+            console.log("case github");
+            var result = url.pathname;
+            console.log(window.location.hostname);
+            result = result.substring(9, result.length);
+        }
+        else if (source.includes(window.location.hostname)) {
+            //for local
+            console.log("case local");
+            var result = url.pathname;
+            console.log(window.location.hostname);
+            result = result.substring(1, result.length);
         }
         else { //path is abosolute path
-            result = url;
+            var result = url;
         }
-        console.log("source: " + result); 
+        console.log("source: " + result);
         //source = "assets/images/rashi/Rashi2.jpeg";
-        const image = document.querySelector(`.spinner > img[src=${CSS.escape(result)}]`);//".spinner > img[src='"+ CSS.escape(source)+"']");
+        const image = document.querySelector(`.spinner > img[src=${CSS.escape(result)}]`);
         // image.src = source;
         // loader = document.querySelector(".spinner");
         image.style.display = "flex";
-        console.log("loading2" + image.src);
-        console.log("closest spinner is " + image.closest(".spinner"));
-        var spinner =  image.closest(".spinner");
+        var spinner = image.closest(".spinner");
         spinner.classList.remove("spinner");
         // loader.classList.remove("img");
-        
+
     });
 }
 
 
-   
+
