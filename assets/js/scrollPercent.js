3d2980c7ded1c6c76113c34e8e1513b167b32795
Jinsang <fldhs2012@gmail.com>
2022-03-03 16:58:22 +0900

    adjusted timeline

---
+++ assets/js/scrollPercent.js
@@ -0,0 +1,61 @@
+const percentLabel = document.querySelectorAll("#percent > li > a");
+$(window).on('scroll', function () {
+    var s = $(window).scrollTop(),
+        d = $(document).height(),
+        c = $(window).height();
+    var scrollPercent = (s / (d - c)) * 100;
+    scrollPercent.toFixed(2);
+    switch (true) {
+        case (scrollPercent < 5): {
+            changeColor();
+            percentLabel[0].style.color = "#ccc";
+            break;
+        }
+
+        case (scrollPercent < 19): {
+            changeColor();
+            percentLabel[1].style.color = "#ccc";
+            break;
+        }
+        case (scrollPercent < 33): {
+            changeColor();
+            percentLabel[2].style.color = "#ccc";
+            break;
+        }
+        case (scrollPercent < 45): {
+            changeColor();
+            percentLabel[3].style.color = "#ccc";
+            break;
+        }
+        case (scrollPercent < 59): {
+            changeColor();
+            percentLabel[4].style.color = "#ccc";
+            break;
+        }
+        case (scrollPercent < 72.5): {
+            changeColor();
+            percentLabel[5].style.color = "#ccc";
+            break;
+        }
+        case (scrollPercent < 86): {
+            changeColor();
+            percentLabel[6].style.color = "#ccc";
+            break;
+        }
+        case (scrollPercent < 110): {
+            changeColor();
+            percentLabel[7].style.color = "#ccc";
+            // i++;j++;                        
+            break;
+        }
+        default: {
+            break;
+        }
+    }
+    function changeColor() {
+        percentLabel.forEach(label => {
+            label.style.color = "rgba(255, 255, 255, 0.2)";
+        });
+    }
+   
+})
