118fd72afa499b99576c562366bbe9687418b26c
Jinsang <fldhs2012@gmail.com>
2022-04-14 02:27:53 +0900

    fixed typo

--- assets/js/currency.js
+++ assets/js/currency.js
@@ -7,9 +7,9 @@ oReq.addEventListener("load", function () {
     myObj = JSON.parse(this.responseText);
     krw = myObj.data.KRW.value.toFixed(2)
 
-    document.querySelector("#jpy").innerHTML = "1 USD: " + myObj.data.JPY.value.toFixed(2) + " JPY"
-    document.querySelector("#cny").innerHTML = "1 USD: " + myObj.data.CNY.value.toFixed(2) + " CNY"
-    document.querySelector("#krw").innerHTML = "1 USD: " + krw +" KRW";
+    document.querySelector("#jpy").innerHTML = "1 USD : " + myObj.data.JPY.value.toFixed(2) + " JPY"
+    document.querySelector("#cny").innerHTML = "1 USD : " + myObj.data.CNY.value.toFixed(2) + " CNY"
+    document.querySelector("#krw").innerHTML = "1 USD : " + krw +" KRW";
     if (krw < 1200) {
         document.querySelector("#exchange").innerHTML = "EXCHANGE RATE LESS THAN 1200. EXCHANGE TO USD NOW!!!";
     }
